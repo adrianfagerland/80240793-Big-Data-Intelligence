5e6859140cbd1a62284fca39370ee7d5f66a0f7a
Adrian Fagerland <adrian.fagerland@hotmail.com>
2023-12-09 09:42:19 +0100

    Refactor: More efficient dataframe operations

--- bdint/models/average.py
+++ bdint/models/average.py
@@ -1,23 +1,18 @@
 import pandas as pd
 
+
 class AverageModel:
-    def __init__(self):
-        self.mean = None  
+    mean = None
 
     def learn(self, x_train_df, y_train_df):
-       self.mean = y_train_df['SalePrice'].mean()
+        self.mean = y_train_df["SalePrice"].mean()
 
     def predict(self, x_test_df):
-       
         if self.mean is None:
             raise ValueError("Model has not been trained. Call 'learn' method first.")
-        
-        
-        result_df = pd.DataFrame(columns=['Id', 'SalePrice'])
-        for index, row in x_test_df.iterrows():
-            id = row['Id']
-            prediction = self.mean
-            result_df.loc[len(result_df)] = {"Id": id, "SalePrice": prediction}
 
+        result_df = pd.DataFrame()
+        result_df["Id"] = x_test_df["Id"]
+        result_df["SalePrice"] = self.mean
 
         return result_df
