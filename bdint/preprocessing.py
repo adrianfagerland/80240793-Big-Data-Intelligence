af26cfa69f4cc1a4b003cb2f298b40f0636334e8
ewa.schoenborn <ewa.schoenborn@gmail.com>
2023-12-19 10:42:52 +0100

    add preprocessing

--- bdint/preprocessing.py
+++ bdint/preprocessing.py
@@ -1,6 +1,17 @@
-def filter_gr_livarea(df):
+def filter_grlivarea(df):
     # the motivation is in the Project Documentation. There is suggested to filter outliers over 4000
     size_A = len(df)
     df = df[df["GrLivArea"] < 4000]
     size_B = len(df)
     return df, (size_A - size_B)
+
+def cast_numerical_to_categorical(df, to_cast: list): #[]'MSSubClass', 'MoSold', 'YrSold']
+    df.loc[:,to_cast] = df.loc[:,to_cast].astype('object')
+    return df
+
+def impute_na_by_none(df, features: list): #['Alley', 'GarageType', 'MiscFeature'] (nominal features)
+    impute = df.loc[:,features]
+    for i in impute.columns:
+        df[i].fillna('None', inplace=True)
+    return df
+
